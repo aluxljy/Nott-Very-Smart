57173a064ecf941fbf97008b3c245e584ddae0a2
Qian Hui <qhliew0302@gmail.com>
2021-11-11 00:31:14 +0800

    main import libraries

--- Nott-Very-Smart_main.py
+++ Nott-Very-Smart_main.py
@@ -1,1 +1,26 @@
-print('this is main')
+# Group Name: Nott-Very-Smart
+# Author: Looi Jie Ying, Liew Qian Hui
+# Project Name: Food Ordering Chatbot
+# Date: 10/11/2021
+
+####################
+# import libraries #
+####################
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+nltk.download('punkt')
+nltk.download('wordnet')
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+import pickle
+
+import json
+with open('intents.json') as file:
+    data = json.load(file)
+
+
