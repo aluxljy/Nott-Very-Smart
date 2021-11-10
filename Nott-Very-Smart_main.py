64d0b1148cdb08115d1d79df692ebd77e39e5a7f
Qian Hui <qhliew0302@gmail.com>
2021-11-11 00:33:11 +0800

    main import libraries

--- Nott-Very-Smart_main.py
+++ Nott-Very-Smart_main.py
@@ -16,8 +16,9 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
-import random
 import pickle
+import random
+
 
 import json
 with open('intents.json') as file:
